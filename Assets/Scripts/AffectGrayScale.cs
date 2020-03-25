20e1c8ac3729ea42fece180670b2a1d966a20db7
kirby <kirby_yu@hotmail.com>
2020-03-25 19:24:47 +0800

    first commit

---
+++ Assets/Scripts/AffectGrayScale.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
+
+public class AffectGrayScale : MonoBehaviour
+{
+    public Toggle toggle;
+
+    GrayScale gs;
+
+    public bool isDead = false;
+    public float speed = 2f;
+
+    void Start()
+    {
+        Volume vol = GetComponent<Volume>();
+
+        GrayScale temp;
+
+        if (vol.profile.TryGet<GrayScale>(out temp))
+        {
+            gs = temp;
+        }
+
+        toggle.onValueChanged.AddListener(OnClickToggle);
+    }
+
+    private void OnClickToggle(bool isOn)
+    {
+        isDead = isOn;
+    }
+
+    void Update()
+    {
+        if(isDead)
+            gs.intensity.value = Mathf.Lerp(gs.intensity.value, 1, Time.deltaTime * speed);
+        else
+            gs.intensity.value = Mathf.Lerp(gs.intensity.value, 0, Time.deltaTime * speed);
+    }
+
+    private void OnDestroy()
+    {
+        toggle.onValueChanged.RemoveListener(OnClickToggle);
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+#  Custom Post Process
+
+Custom GrayScale Effect
+
+
+
+**Unity 2019.3.2f1 or newer is required.**
